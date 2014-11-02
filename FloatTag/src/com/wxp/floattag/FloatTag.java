b904a6966d10913e289c87b666af3e3205dfe8d3
wangxianpeng <xianpeng.wang@nbbsw.com>
2014-11-02 12:37:12 +0800

    floattag

---
+++ FloatTag/src/com/wxp/floattag/FloatTag.java
@@ -0,0 +1,65 @@
+package com.wxp.floattag;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+public class FloatTag extends LinearLayout {
+
+	private ScrollView mScrollView;
+	private View mFloatTagView;
+	
+	private FloatTagStateListener mFloatTagStateListener;
+	
+	boolean mScrollUp=true;
+	public FloatTag(Context context){
+		this(context, null);
+	}
+	public FloatTag(Context context, AttributeSet attrs) {
+		super(context, attrs);		
+		setOrientation(LinearLayout.VERTICAL);
+	}
+	
+	@Override
+	public void computeScroll() {
+		// TODO Auto-generated method stub
+		super.computeScroll();
+	    if(mScrollView!=null && mFloatTagView!=null && mFloatTagStateListener!=null){
+	    	int scrollViewY=mScrollView.getScrollY();
+	    	int floatTagTopY=mFloatTagView.getTop();
+	    	Log.e("wxp", scrollViewY+" , "+floatTagTopY);
+	    	if(mScrollUp){
+	    		if(scrollViewY>=floatTagTopY){
+	    			mFloatTagStateListener.onFloatTagShow();
+
+	    			mScrollUp=false;
+	    		}
+	    	} else {
+	    		if(scrollViewY<=floatTagTopY){
+	    			mFloatTagStateListener.onFloatTagHide();
+
+	    			mScrollUp=true;
+	    		}
+	    	}
+	    }
+	
+	}
+	
+	public void init(ScrollView scrollView,View floatTagView,FloatTagStateListener listener){
+		this.mScrollView=scrollView;
+		this.mFloatTagView=floatTagView;
+		this.mFloatTagStateListener=listener;
+
+	}
+
+	public void setFloatTagStateListener(FloatTagStateListener listener){
+		this.mFloatTagStateListener=listener;
+	}
+	public interface FloatTagStateListener{
+        public void onFloatTagShow();
+        public void onFloatTagHide();
+	}
+}
--- FloatTag/src/com/wxp/floattag/MainActivity.java
+++ FloatTag/src/com/wxp/floattag/MainActivity.java
@@ -1,15 +1,42 @@
 package com.wxp.floattag;
 
+import com.wxp.floattag.FloatTag.FloatTagStateListener;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 
 public class MainActivity extends Activity {
 
+	ScrollView mScrollView;
+
+	FloatTag mFloatTag;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mFloatTag=(FloatTag)findViewById(R.id.id_ft_floattag);
+	   mScrollView=(ScrollView)findViewById(R.id.id_sv_mainscrolleview);
+
+	   /*
+	    * 注意：这里的第二个参数是初始化就就现实出来的，而下面控制隐藏和显示的是另一个View
+	    */
+	   mFloatTag.init(mScrollView, findViewById(R.id.id_btn_tag), new FloatTagStateListener() {
+		
+		@Override
+		public void onFloatTagShow() {
+			findViewById(R.id.id_btn_float_tag).setVisibility(View.VISIBLE);			
+		}
+		
+		@Override
+		public void onFloatTagHide() {
+			findViewById(R.id.id_btn_float_tag).setVisibility(View.GONE);		
+		}
+	});
+	
 	}
 
 	@Override
